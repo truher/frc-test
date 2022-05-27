68ec2dc338e03638b226d875e430ca9c82595a4a
Joel Truher <joel@truher.org>
2022-05-26 20:40:23 -0700

    gyro actually produces the correct radians/sec

--- servo-example-2/src/main/java/frc/sensors/LSM6DSOX_I2C.java
+++ servo-example-2/src/main/java/frc/sensors/LSM6DSOX_I2C.java
@@ -3,12 +3,9 @@ package frc.sensors;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
  * Adafruit 4517 contains this gyro, interfaced with I2C.
@@ -26,6 +23,9 @@ public class LSM6DSOX_I2C implements Sendable {
     // private static final byte LSM6DSOX_OUTY_L_G = (byte) 0x24;
     // for now i only care about yaw
     private static final byte LSM6DSOX_OUTZ_L_G = (byte) 0x26;
+
+    // seems like raw is about 50 quiescent, so remove it.
+    private static final int kRawOffset = -50;
 
     // Output Data Rate
     // these are the high 4 bits in CTRL2_G
@@ -56,23 +56,26 @@ public class LSM6DSOX_I2C implements Sendable {
     // Sensitivity
     // these are bits 1-3 in CTRL2_G
     public enum LSM6DSOX_FS_G_T {
-        LSM6DSOX_125dps(0b0000_0010),
-        LSM6DSOX_250dps(0b0000_0000),
-        LSM6DSOX_500dps(0b0000_0100),
-        LSM6DSOX_1000dps(0b0000_1000),
-        LSM6DSOX_2000dps(0b0000_1100);
+        LSM6DSOX_125dps(0b0000_0010, 4.375),
+        LSM6DSOX_250dps(0b0000_0000, 8.75),
+        LSM6DSOX_500dps(0b0000_0100, 17.5),
+        LSM6DSOX_1000dps(0b0000_1000, 35),
+        LSM6DSOX_2000dps(0b0000_1100, 70);
 
         public final byte value;
+        public final double mdps;
         // Mask to erase the relevant bits.
         // (Note the ST code doesn't use a mask, it uses bit fields in a struct.)
         public static final byte mask = (byte) 0b0000_1110;
 
-        private LSM6DSOX_FS_G_T(int value) {
+        private LSM6DSOX_FS_G_T(int value, double mdps) {
             this.value = (byte) value;
+            this.mdps = mdps;
         }
     }
 
     private final I2C m_i2c;
+    private LSM6DSOX_FS_G_T m_scale;
 
     public LSM6DSOX_I2C() {
         m_i2c = new I2C(I2C.Port.kMXP, LSM6DSOX_I2C_ADD_L >>> 1); // shift addr!
@@ -81,9 +84,8 @@ public class LSM6DSOX_I2C implements Sendable {
     }
 
     private void setGyroDataRate(LSM6DSOX_ODR_G_T data_rate) {
-        // TODO: make this a member
         ByteBuffer buf = ByteBuffer.allocate(1);
-        buf.order(ByteOrder.LITTLE_ENDIAN); // TODO: check
+        buf.order(ByteOrder.LITTLE_ENDIAN);
         m_i2c.read(LSM6DSOX_CTRL2_G, 1, buf);
         byte ctrl2 = buf.get();
         ctrl2 &= ~LSM6DSOX_ODR_G_T.mask;
@@ -91,8 +93,8 @@ public class LSM6DSOX_I2C implements Sendable {
         m_i2c.write(LSM6DSOX_CTRL2_G, ctrl2);
     }
 
-    // TODO: extract some common parts of these setters
     private void setGyroScale(LSM6DSOX_FS_G_T scale) {
+        m_scale = scale;
         ByteBuffer buf = ByteBuffer.allocate(1);
         buf.order(ByteOrder.LITTLE_ENDIAN);
         m_i2c.read(LSM6DSOX_CTRL2_G, 1, buf);
@@ -102,19 +104,21 @@ public class LSM6DSOX_I2C implements Sendable {
         m_i2c.write(LSM6DSOX_CTRL2_G, ctrl2);
     }
 
-    // radians per second
+    /**
+     * Yaw rate in radians/sec
+     */
     public double getRate() {
-        // at 250dpsd full scale, 16 bits signed, times pi/180
-        // TODO: use the config for this
-        return (double) getYawRateRaw()  * 0.0001332;
+        return (double) getYawRateRaw() * m_scale.mdps * Math.PI / 180000;
     }
 
-    // unit depends on config
+    /**
+     * Yaw rate unit depends on config
+     */
     public int getYawRateRaw() {
         ByteBuffer buf = ByteBuffer.allocate(2);
-        buf.order(ByteOrder.LITTLE_ENDIAN); 
+        buf.order(ByteOrder.LITTLE_ENDIAN);
         m_i2c.read(LSM6DSOX_OUTZ_L_G, 2, buf);
-        return -1 * buf.getShort();
+        return -1 * (buf.getShort() - kRawOffset);
     }
 
     @Override
