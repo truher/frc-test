0c80be19f4ae26672bfd58bc2fe2a7669699608e
Joel Truher <joel@truher.org>
2022-05-27 22:33:37 -0700

    turn on optimization to reduce module turns

--- servo-example-2/src/main/java/frc/robot/subsystems/Module.java
+++ servo-example-2/src/main/java/frc/robot/subsystems/Module.java
@@ -3,17 +3,19 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
+/**
+ * A swerve module comprising a driving component and a turning component.
+ */
 public class Module {
     public final Turner m_steer;
     public final Driver m_drive;
 
     /**
-     * Offset is measured in sensor units, [0,1]. To adjust the module zero in the
-     * positive (anticlockwise) direction, reduce the offset.
-     * 
-     * @param steerChannel
-     * @param steerOffset
-     * @param driveChannel
+     * @param steerChannel Same channel for PWM (motor) and DIO (duty cycle encoder)
+     * @param steerOffset  Offset is measured in sensor units, [0,1]. To adjust the
+     *                     module zero in the
+     *                     positive (anticlockwise) direction, reduce the offset.
+     * @param driveChannel Same channel for PWM (motor) and DIO (duty cycle encoder)
      */
     public Module(int steerChannel, double steerOffset, int driveChannel) {
         m_steer = new Turner(steerChannel, steerOffset);
@@ -21,17 +23,13 @@ public class Module {
     }
 
     public SwerveModuleState getState() {
-        int driveMetersPerSec = 0;
-        int angle = 0;
-        return new SwerveModuleState(driveMetersPerSec, new Rotation2d(angle));
-        // return new SwerveModuleState(m_driveEncoder.getRate(), new
-        // Rotation2d(m_turningEncoder.get()));
+        return new SwerveModuleState(m_drive.getMeasurement(),
+                new Rotation2d(m_steer.getPosition()));
     }
 
     public void setDesiredState(SwerveModuleState desiredState) {
-        //SwerveModuleState state = SwerveModuleState.optimize(desiredState,
-        //        new Rotation2d(m_steer.getMeasurement()));
-        SwerveModuleState state = desiredState;
+        SwerveModuleState state = SwerveModuleState.optimize(desiredState,
+                new Rotation2d(m_steer.getPosition()));
         m_drive.setSetpoint(state.speedMetersPerSecond);
         m_steer.setGoal(state.angle.getRadians());
     }
