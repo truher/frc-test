d0041f70c773b1f616e38114106f93ec05089fe6
Joel Truher <joel@truher.org>
2022-06-15 18:29:11 -0700

    added some encoders and dashboard

--- swerve2/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ swerve2/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,19 +4,59 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+    public final AnalogEncoder ch0;
+    public final AnalogEncoder ch1;
+    public final AnalogEncoder ch2;
+    public final AnalogEncoder ch3;
+    public  AHRS ahrs;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem() {
+        ch0 = new AnalogEncoder(0);
+        ch1 = new AnalogEncoder(1);
+        ch2 = new AnalogEncoder(2);
+        ch3 = new AnalogEncoder(3);
+        try {
+            ahrs = new AHRS(Port.kUSB);
+        } catch (RuntimeException ex) {
+            DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+        }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+        SmartDashboard.putData("blarg", this);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("blarg");
+        builder.addDoubleProperty("ch0", () -> ch0.getAbsolutePosition(), null);
+        builder.addDoubleProperty("ch1", () -> ch1.getAbsolutePosition(), null);
+        builder.addDoubleProperty("ch2", () -> ch2.getAbsolutePosition(), null);
+        builder.addDoubleProperty("ch3", () -> ch3.getAbsolutePosition(), null);
+        builder.addDoubleProperty("ch0 offset", () -> ch0.getPositionOffset(), null);
+        builder.addDoubleProperty("ch1 offset", () -> ch1.getPositionOffset(), null);
+        builder.addDoubleProperty("ch2 offset", () -> ch2.getPositionOffset(), null);
+        builder.addDoubleProperty("ch3 offset", () -> ch3.getPositionOffset(), null);
+        builder.addDoubleProperty("gyro", () -> ahrs.getAngle(), null);
+
+    }
 }
