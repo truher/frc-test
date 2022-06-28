165e1016096349a6abca2c83ce0ce8812c05119b
Joel Truher <joel@truher.org>
2022-06-28 07:52:51 -0700

    cleanup

--- cv1/src/test/java/TestCV.java
+++ cv1/src/test/java/TestCV.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.Test;
+//import org.junit.Test;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
--- cv1/src/test/java/TestMyCalib3d.java
+++ cv1/src/test/java/TestMyCalib3d.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+//import org.junit.Test;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
