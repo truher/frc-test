7d4916dc9959e9951c9b8d335c1502a4c557a118
Joel Truher <joel@truher.org>
2022-07-13 18:53:23 -0700

    adding steering motor controllers

--- swerve2/src/main/java/frc/robot/RobotContainer.java
+++ swerve2/src/main/java/frc/robot/RobotContainer.java
@@ -4,45 +4,71 @@
 
 package frc.robot;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
-public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+public class RobotContainer implements Sendable {
+    private final ExampleSubsystem m_exampleSubsystem;
+    private final Joystick m_joystick;
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+    public RobotContainer() {
+        configureButtonBindings();
+        m_joystick = new Joystick(2);
+        m_exampleSubsystem = new ExampleSubsystem();
+        m_exampleSubsystem.setDefaultCommand(new RunCommand(
+                () -> m_exampleSubsystem.doit(m_joystick.getX()),
+                m_exampleSubsystem));
+        SmartDashboard.putData("robot container", this);
+    }
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+    }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("container");
+        builder.addDoubleProperty("X (positive right)", () -> m_joystick.getX(), null);
+        builder.addDoubleProperty("Y (positive back)", () -> m_joystick.getY(), null);
+        builder.addDoubleProperty("Twist (positive right)", () -> m_joystick.getTwist(), null);
+        builder.addDoubleProperty("Throttle (positive back)", () -> m_joystick.getThrottle(), null);
+        builder.addDoubleProperty("Button Count", () -> m_joystick.getButtonCount(), null);
+        // these assignments all seem like they're maybe off by one?
+        builder.addBooleanProperty("Button 0 (?)", () -> m_joystick.getRawButton(0), null);
+        builder.addBooleanProperty("Button 1 (trigger)", () -> m_joystick.getRawButton(1), null);
+        builder.addBooleanProperty("Button 2 (thumb)", () -> m_joystick.getRawButton(2), null);
+        builder.addBooleanProperty("Button 3 (top left rear)", () -> m_joystick.getRawButton(3), null);
+        builder.addBooleanProperty("Button 4 (top right rear)", () -> m_joystick.getRawButton(4), null);
+        builder.addBooleanProperty("Button 5 (top left front)", () -> m_joystick.getRawButton(5), null);
+        builder.addBooleanProperty("Button 6 (top right front)", () -> m_joystick.getRawButton(6), null);
+        builder.addBooleanProperty("Button 7 (base front left)", () -> m_joystick.getRawButton(7), null);
+        builder.addBooleanProperty("Button 8 (base front right)", () -> m_joystick.getRawButton(8), null);
+        builder.addBooleanProperty("Button 9 (base middle left)", () -> m_joystick.getRawButton(9), null);
+        builder.addBooleanProperty("Button 10 (base middle right)", () -> m_joystick.getRawButton(10), null);
+        builder.addBooleanProperty("Button 11 (base rear left)", () -> m_joystick.getRawButton(11), null);
+    }
 }
--- swerve2/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ swerve2/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -10,29 +10,44 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
-    public final AnalogEncoder ch0;
+    public final AnalogEncoder a0;
     public final AnalogEncoder ch1;
     public final AnalogEncoder ch2;
     public final AnalogEncoder ch3;
     public AHRS ahrs;
+    public final PWMMotorController m0;
+    public final PWMMotorController m1;
+    public final PWMMotorController m2;
+    public final PWMMotorController m3;
 
     /** Creates a new ExampleSubsystem. */
     public ExampleSubsystem() {
-        ch0 = new AnalogEncoder(0);
+        a0 = new AnalogEncoder(0);
         ch1 = new AnalogEncoder(1);
         ch2 = new AnalogEncoder(2);
         ch3 = new AnalogEncoder(3);
+        m0 = new VictorSP(0);
+        m1 = new VictorSP(1);
+        m2 = new VictorSP(2);
+        m3 = new VictorSP(3);
+
         try {
             ahrs = new AHRS(Port.kUSB);
         } catch (RuntimeException ex) {
             DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
         }
 
-        SmartDashboard.putData("blarg", this);
+        SmartDashboard.putData("example subsystem", this);
+    }
+
+    public void doit(double blarg) {
+        // nothing
     }
 
     @Override
@@ -48,11 +63,11 @@ public class ExampleSubsystem extends SubsystemBase {
     @Override
     public void initSendable(SendableBuilder builder) {
         builder.setSmartDashboardType("blarg");
-        builder.addDoubleProperty("ch0", () -> ch0.getAbsolutePosition(), null);
+        builder.addDoubleProperty("ch0", () -> a0.getAbsolutePosition(), null);
         builder.addDoubleProperty("ch1", () -> ch1.getAbsolutePosition(), null);
         builder.addDoubleProperty("ch2", () -> ch2.getAbsolutePosition(), null);
         builder.addDoubleProperty("ch3", () -> ch3.getAbsolutePosition(), null);
-        builder.addDoubleProperty("ch0 offset", () -> ch0.getPositionOffset(), null);
+        builder.addDoubleProperty("ch0 offset", () -> a0.getPositionOffset(), null);
         builder.addDoubleProperty("ch1 offset", () -> ch1.getPositionOffset(), null);
         builder.addDoubleProperty("ch2 offset", () -> ch2.getPositionOffset(), null);
         builder.addDoubleProperty("ch3 offset", () -> ch3.getPositionOffset(), null);
