73b53364060f18572d2f37bd39cd25546f5878c5
Joel Truher <joel@truher.org>
2022-07-14 21:51:09 -0700

    add talon stuff, remove extra stuff

--- swerve2/src/main/java/frc/robot/RobotContainer.java
+++ swerve2/src/main/java/frc/robot/RobotContainer.java
@@ -1,74 +1,99 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer implements Sendable {
     private final ExampleSubsystem m_exampleSubsystem;
-    private final Joystick m_joystick;
+    private final XboxController m_driverController;
 
     public RobotContainer() {
         configureButtonBindings();
-        m_joystick = new Joystick(2);
+        m_driverController = new XboxController(0);
         m_exampleSubsystem = new ExampleSubsystem();
         m_exampleSubsystem.setDefaultCommand(new RunCommand(
-                () -> m_exampleSubsystem.doit(m_joystick.getX()),
+                () -> {
+                },
                 m_exampleSubsystem));
         SmartDashboard.putData("robot container", this);
     }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
     private void configureButtonBindings() {
+    }
+
+    public Command getAutonomousCommand() {
+        return new RunCommand(() -> {
+        }, m_exampleSubsystem);
+    }
+
+    public void runTest() {
+        boolean rearLeft = m_driverController.getAButton();
+        boolean rearRight = m_driverController.getBButton();
+        boolean frontLeft = m_driverController.getXButton();
+        boolean frontRight = m_driverController.getYButton();
+        double driveControl = m_driverController.getLeftY();
+        double turnControl = m_driverController.getLeftX();
+        double[][] desiredOutputs = {
+                { frontLeft ? driveControl : 0, frontLeft ? turnControl : 0 },
+                { frontRight ? driveControl : 0, frontRight ? turnControl : 0 },
+                { rearLeft ? driveControl : 0, rearLeft ? turnControl : 0 },
+                { rearRight ? driveControl : 0, rearRight ? turnControl : 0 }
+        };
+        m_exampleSubsystem.test(desiredOutputs);
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
         builder.setSmartDashboardType("container");
-        builder.addDoubleProperty("X (positive right)", () -> m_joystick.getX(), null);
-        builder.addDoubleProperty("Y (positive back)", () -> m_joystick.getY(), null);
-        builder.addDoubleProperty("Twist (positive right)", () -> m_joystick.getTwist(), null);
-        builder.addDoubleProperty("Throttle (positive back)", () -> m_joystick.getThrottle(), null);
-        builder.addDoubleProperty("Button Count", () -> m_joystick.getButtonCount(), null);
-        // these assignments all seem like they're maybe off by one?
-        builder.addBooleanProperty("Button 0 (?)", () -> m_joystick.getRawButton(0), null);
-        builder.addBooleanProperty("Button 1 (trigger)", () -> m_joystick.getRawButton(1), null);
-        builder.addBooleanProperty("Button 2 (thumb)", () -> m_joystick.getRawButton(2), null);
-        builder.addBooleanProperty("Button 3 (top left rear)", () -> m_joystick.getRawButton(3), null);
-        builder.addBooleanProperty("Button 4 (top right rear)", () -> m_joystick.getRawButton(4), null);
-        builder.addBooleanProperty("Button 5 (top left front)", () -> m_joystick.getRawButton(5), null);
-        builder.addBooleanProperty("Button 6 (top right front)", () -> m_joystick.getRawButton(6), null);
-        builder.addBooleanProperty("Button 7 (base front left)", () -> m_joystick.getRawButton(7), null);
-        builder.addBooleanProperty("Button 8 (base front right)", () -> m_joystick.getRawButton(8), null);
-        builder.addBooleanProperty("Button 9 (base middle left)", () -> m_joystick.getRawButton(9), null);
-        builder.addBooleanProperty("Button 10 (base middle right)", () -> m_joystick.getRawButton(10), null);
-        builder.addBooleanProperty("Button 11 (base rear left)", () -> m_joystick.getRawButton(11), null);
+        builder.addDoubleProperty("right y", () -> m_driverController.getRightY(), null);
+        builder.addDoubleProperty("right x", () -> m_driverController.getRightX(), null);
+        builder.addDoubleProperty("left x", () -> m_driverController.getLeftX(), null);
     }
+
+    // this is the joystick version. i like the xbox version better, but it took
+    // awhile to figure out which button was which.
+    // @Override
+    // public void initSendable(SendableBuilder builder) {
+    // builder.setSmartDashboardType("container");
+    // builder.addDoubleProperty("X (positive right)", () -> m_joystick.getX(),
+    // null);
+    // builder.addDoubleProperty("Y (positive back)", () -> m_joystick.getY(),
+    // null);
+    // builder.addDoubleProperty("Twist (positive right)", () ->
+    // m_joystick.getTwist(), null);
+    // builder.addDoubleProperty("Throttle (positive back)", () ->
+    // m_joystick.getThrottle(), null);
+    // builder.addDoubleProperty("Button Count", () -> m_joystick.getButtonCount(),
+    // null);
+    // // these assignments all seem like they're maybe off by one?
+    // builder.addBooleanProperty("Button 0 (?)", () -> m_joystick.getRawButton(0),
+    // null);
+    // builder.addBooleanProperty("Button 1 (trigger)", () ->
+    // m_joystick.getRawButton(1), null);
+    // builder.addBooleanProperty("Button 2 (thumb)", () ->
+    // m_joystick.getRawButton(2), null);
+    // builder.addBooleanProperty("Button 3 (top left rear)", () ->
+    // m_joystick.getRawButton(3), null);
+    // builder.addBooleanProperty("Button 4 (top right rear)", () ->
+    // m_joystick.getRawButton(4), null);
+    // builder.addBooleanProperty("Button 5 (top left front)", () ->
+    // m_joystick.getRawButton(5), null);
+    // builder.addBooleanProperty("Button 6 (top right front)", () ->
+    // m_joystick.getRawButton(6), null);
+    // builder.addBooleanProperty("Button 7 (base front left)", () ->
+    // m_joystick.getRawButton(7), null);
+    // builder.addBooleanProperty("Button 8 (base front right)", () ->
+    // m_joystick.getRawButton(8), null);
+    // builder.addBooleanProperty("Button 9 (base middle left)", () ->
+    // m_joystick.getRawButton(9), null);
+    // builder.addBooleanProperty("Button 10 (base middle right)", () ->
+    // m_joystick.getRawButton(10), null);
+    // builder.addBooleanProperty("Button 11 (base rear left)", () ->
+    // m_joystick.getRawButton(11), null);
+    // }
 }
--- swerve2/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ swerve2/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -1,9 +1,9 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,54 +20,59 @@ public class ExampleSubsystem extends SubsystemBase {
     public final AnalogEncoder m_steeringEncoder1;
     public final AnalogEncoder m_steeringEncoder2;
     public final AnalogEncoder m_steeringEncoder3;
-    public AHRS ahrs;
     public final PWMMotorController m_steeringMotor0;
     public final PWMMotorController m_steeringMotor1;
     public final PWMMotorController m_steeringMotor2;
     public final PWMMotorController m_steeringMotor3;
-    public final Talon m_driveMotor11;
-    public final Talon m_driveMotor12;
-    public final Talon m_driveMotor21;
-    public final Talon m_driveMotor22;
+    public final WPI_TalonFX m_driveMotor11;
+    public final WPI_TalonFX m_driveMotor12;
+    public final WPI_TalonFX m_driveMotor21;
+    public final WPI_TalonFX m_driveMotor22;
+    public AHRS ahrs;
 
-
-
-    /** Creates a new ExampleSubsystem. */
     public ExampleSubsystem() {
         m_steeringEncoder0 = new AnalogEncoder(0);
         m_steeringEncoder1 = new AnalogEncoder(1);
         m_steeringEncoder2 = new AnalogEncoder(2);
         m_steeringEncoder3 = new AnalogEncoder(3);
+
         m_steeringMotor0 = new VictorSP(0);
         m_steeringMotor1 = new VictorSP(1);
         m_steeringMotor2 = new VictorSP(2);
         m_steeringMotor3 = new VictorSP(3);
-        m_driveMotor11 = new Talon(11);
-        m_driveMotor12 = new Talon(12);
-        m_driveMotor21 = new Talon(21);
-        m_driveMotor22 = new Talon(22);
+
+        m_driveMotor11 = new WPI_TalonFX(11);
+        m_driveMotor12 = new WPI_TalonFX(12);
+        m_driveMotor21 = new WPI_TalonFX(21);
+        m_driveMotor22 = new WPI_TalonFX(22);
+
+        // by default, feedback comes only every 250ms, which seems too slow.
+        m_driveMotor11.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 20);
+        m_driveMotor12.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 20);
+        m_driveMotor21.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 20);
+        m_driveMotor22.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 20);
+
+        m_driveMotor11.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 5, 5, 0));
+        m_driveMotor12.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 5, 5, 0));
+        m_driveMotor21.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 5, 5, 0));
+        m_driveMotor22.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 5, 5, 0));
+
+        m_driveMotor11.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 5, 5, 0));
+        m_driveMotor12.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 5, 5, 0));
+        m_driveMotor21.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 5, 5, 0));
+        m_driveMotor22.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 5, 5, 0));
 
         try {
             ahrs = new AHRS(Port.kUSB);
         } catch (RuntimeException ex) {
             DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+            throw ex;
         }
-
         SmartDashboard.putData("example subsystem", this);
-    }
-
-    public void doit(double blarg) {
-        // nothing
     }
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
-    }
-
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
     }
 
     @Override
@@ -82,7 +86,37 @@ public class ExampleSubsystem extends SubsystemBase {
         builder.addDoubleProperty("ch1 offset", () -> m_steeringEncoder1.getPositionOffset(), null);
         builder.addDoubleProperty("ch2 offset", () -> m_steeringEncoder2.getPositionOffset(), null);
         builder.addDoubleProperty("ch3 offset", () -> m_steeringEncoder3.getPositionOffset(), null);
+        builder.addDoubleProperty("ch0 throttle", () -> m_steeringMotor0.get(), null);
+        builder.addDoubleProperty("ch1 throttle", () -> m_steeringMotor1.get(), null);
+        builder.addDoubleProperty("ch2 throttle", () -> m_steeringMotor2.get(), null);
+        builder.addDoubleProperty("ch3 throttle", () -> m_steeringMotor3.get(), null);
+        // velocity is 1/2048ths of a turn per 100ms
+        builder.addDoubleProperty("drive11 velocity",
+                () -> m_driveMotor11.getSensorCollection().getIntegratedSensorVelocity(), null);
+        builder.addDoubleProperty("drive12 velocity",
+                () -> m_driveMotor12.getSensorCollection().getIntegratedSensorVelocity(), null);
+        builder.addDoubleProperty("drive21 velocity",
+                () -> m_driveMotor21.getSensorCollection().getIntegratedSensorVelocity(), null);
+        builder.addDoubleProperty("drive22 velocity",
+                () -> m_driveMotor22.getSensorCollection().getIntegratedSensorVelocity(), null);
+        builder.addDoubleProperty("drive11 throttle", () -> m_driveMotor11.get(), null);
+        builder.addDoubleProperty("drive12 throttle", () -> m_driveMotor12.get(), null);
+        builder.addDoubleProperty("drive21 throttle", () -> m_driveMotor21.get(), null);
+        builder.addDoubleProperty("drive22 throttle", () -> m_driveMotor22.get(), null);
+
         builder.addDoubleProperty("gyro", () -> ahrs.getAngle(), null);
+
+    }
+
+    public void test(double[][] desiredOutputs) {
+        m_driveMotor11.set(desiredOutputs[0][0]);
+        m_steeringMotor0.set(desiredOutputs[0][1]);
+        m_driveMotor12.set(desiredOutputs[1][0]);
+        m_steeringMotor1.set(desiredOutputs[1][1]);
+        m_driveMotor21.set(desiredOutputs[2][0]);
+        m_steeringMotor2.set(desiredOutputs[2][1]);
+        m_driveMotor22.set(desiredOutputs[3][0]);
+        m_steeringMotor3.set(desiredOutputs[3][1]);
 
     }
 }
