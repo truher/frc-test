55fa769259f918ea934d5c48597e116d8c7b49b9
Joel Truher <joel@truher.org>
2022-09-13 16:15:02 -0700

    wheels now work open-loop

--- shooter-test/src/main/java/frc/robot/Robot.java
+++ shooter-test/src/main/java/frc/robot/Robot.java
@@ -11,22 +11,22 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot implements Sendable {
-    private static final int FEED_DURATION = 50;
-    private static final double FEED_SPEED = 0.2;
-    private static final double ELEVATION_SPEED = 0.01;
+
+    // LATER: put this stuff in like a subsystem or command or whatever
+    // FEED
+    private static final int FEED_DURATION = 7;
+    private static final double FEED_SPEED = 1.0;
 
     private class Feed extends PWMMotorController {
         protected Feed(String name, int channel) {
             super(name, channel);
-            m_pwm.setBounds(1.72, 1.52, 1.5, 1.48, 1.28);
+            m_pwm.setBounds(1.72, 1.52, 1.5, 1.48, 1.28); // parallax360
             m_pwm.setPeriodMultiplier(PWM.PeriodMultiplier.k4X);
             m_pwm.setSpeed(0.0);
             m_pwm.setZeroLatch();
         }
     }
 
-    // LATER: put this stuff in like a subsystem or command or whatever
-    // FEED
     private final Feed m_feed = new Feed("feed", 7);
     boolean m_advance_feed = false;
 
@@ -38,14 +38,39 @@ public class Robot extends TimedRobot implements Sendable {
     private Feedstate m_feedstate = Feedstate.OFF;
 
     // ELEVATION
+    private static final double ELEVATION_SPEED = 0.01;
     int m_elevation_up_down = -1;
     private final Servo m_elevation = new Servo(6);
     // LATER: calibrate elevation
     private double m_current_elevation = 0.5;
 
     // WHEELS
+    private static final int WHEEL_FULL = 4095;
+
+    private class Wheel extends PWMMotorController {
+        protected Wheel(String name, int channel) {
+            super(name, channel);
+            // m_pwm.setBounds(5.0, 0.0, 0.0, 0.0, 0.0);
+            m_pwm.setPeriodMultiplier(PWM.PeriodMultiplier.k1X);
+            // m_pwm.setSpeed(0.0);
+            m_pwm.setRaw(0);
+            // m_pwm.setZeroLatch();
+        }
+
+        public void setRaw(int value) {
+            m_pwm.setRaw(value);
+            feed();
+        }
+
+        public int getRaw() {
+            return m_pwm.getRaw();
+        }
+    }
+
     double m_wheel_speed = 0;
-    private final PWMMotorController m_wheels = new PWMMotorController("wheel", 8) {
+    private final Wheel m_wheel1 = new Wheel("wheel1", 8) {
+    };
+    private final Wheel m_wheel2 = new Wheel("wheel2", 9) {
     };
 
     private final XboxController m_controller = new XboxController(0);
@@ -60,7 +85,7 @@ public class Robot extends TimedRobot implements Sendable {
 
     @Override
     public void teleopInit() {
-        m_feedstate = Feedstate.OFF; // back up a bit to reset the ball position
+        m_feedstate = Feedstate.REVERSING; // back up a bit to reset the ball position
         m_state_counter = 0;
     }
 
@@ -82,7 +107,19 @@ public class Robot extends TimedRobot implements Sendable {
         }
         if (m_state_counter > FEED_DURATION) { // stop after 1s
             m_state_counter = 0;
-            m_feedstate = Feedstate.OFF;
+            switch (m_feedstate) {
+                case OFF:
+                    break;
+                case REVERSING:
+                    m_feedstate = Feedstate.OFF;
+                    break;
+                case ADVANCING:
+                    m_feedstate = Feedstate.REVERSING; // reset position
+                    break;
+                default:
+                    m_feedstate = Feedstate.OFF;
+            }
+
         }
         switch (m_feedstate) {
             case OFF:
@@ -110,7 +147,8 @@ public class Robot extends TimedRobot implements Sendable {
 
         // WHEELS
         m_wheel_speed = m_controller.getLeftTriggerAxis(); // wheel speed
-        m_wheels.set(m_wheel_speed);
+        m_wheel1.setRaw((int) (m_wheel_speed * WHEEL_FULL));
+        m_wheel2.setRaw((int) (m_wheel_speed * WHEEL_FULL));
     }
 
     @Override
@@ -130,6 +168,10 @@ public class Robot extends TimedRobot implements Sendable {
 
         // WHEELS
         builder.addDoubleProperty("m_wheel_speed", () -> m_wheel_speed, null);
-        builder.addDoubleProperty("m_wheels", () -> m_wheels.get(), null);
+        builder.addDoubleProperty("m_wheel1", () -> m_wheel1.get(), null);
+        builder.addDoubleProperty("m_wheel2", () -> m_wheel2.get(), null);
+        builder.addDoubleProperty("m_wheel1 raw", () -> m_wheel1.getRaw(), null);
+        builder.addDoubleProperty("m_wheel2 raw", () -> m_wheel2.getRaw(), null);
+
     }
 }
