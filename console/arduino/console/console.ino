a72086e74a38b51dffc578ed7c810543ec9dd621
Joel Truher <joel@truher.org>
2022-10-12 22:53:20 -0700

    verify 9 axes and 32 buttons

--- console/arduino/console/console.ino
+++ console/arduino/console/console.ino
@@ -1,7 +1,12 @@
 #include <Joystick.h>  // see https://github.com/MHeironimus/ArduinoJoystickLibrary
 
-const int ledPin = 13;                                               // the built-in LED
-const int buttonPins[] = { 12, 12, 12, 12 };                         // Arduino pin numbers, up to 32 pins
+const int ledPin = 13;  // the built-in LED
+
+// Arduino pin numbers, up to 32 pins
+const int buttonPins[] = { 12, 12, 12, 12, 12, 12, 12, 12,
+                           12, 12, 12, 12, 12, 12, 12, 12,
+                           12, 12, 12, 12, 12, 12, 12, 12,
+                           12, 12, 12, 12, 12, 12, 12, 12 };
 const int buttonCount = sizeof(buttonPins) / sizeof(buttonPins[0]);  // max 32
 const int reportId = 0x03;                                           // this is the default
 
@@ -9,7 +14,23 @@ bool buttonStates[buttonCount] = {};
 int axisSetting = 0;
 
 // The class name really has a trailing underscore.  :-(
-Joystick_ m_joystick(reportId, JOYSTICK_TYPE_JOYSTICK, buttonCount);
+// while 11 channels are possible here, only 9 are allowed by the driver station.
+// note that the names used here don't necessarily correspond to the names in the DS.
+Joystick_ m_joystick(reportId,
+                     JOYSTICK_TYPE_JOYSTICK,
+                     buttonCount,
+                     2,       // hatswitches, 2 is max
+                     true,    // x
+                     true,    // y
+                     true,    // z
+                     true,    // rx
+                     true,    // ry
+                     true,    // rz
+                     true,    // rudder
+                     true,    // throttle
+                     true,    // accel
+                     false,   // brake
+                     false);  // steer
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // indicate updates
@@ -26,22 +47,21 @@ void loop() {
     if (desiredButtonState != buttonStates[i]) {
       buttonStates[i] = desiredButtonState;
       m_joystick.setButton(i, desiredButtonState);
-      axisSetting = axisSetting + 10;
+      axisSetting = axisSetting + 10; // exercise the axes
       if (axisSetting > 1023) {
         axisSetting = 0;
       }
-      // the axes have names instead of numbers.  :-(
-      m_joystick.setXAxis(axisSetting);        // axis 0
-      m_joystick.setYAxis(axisSetting);        // axis 1
-      m_joystick.setZAxis(axisSetting);        // axis 2
-      m_joystick.setRxAxis(axisSetting);       // axis 3
-      m_joystick.setRyAxis(axisSetting);       // axis 4
-      m_joystick.setRzAxis(axisSetting);       // axis 5
-      m_joystick.setThrottle(axisSetting);     // axis 6
-      m_joystick.setRudder(axisSetting);       // axis 7
-      m_joystick.setSteering(axisSetting);     // axis 8
-      m_joystick.setAccelerator(axisSetting);  // axis 9
-      m_joystick.setBrake(axisSetting);        // axis 10
+      m_joystick.setXAxis(axisSetting);
+      m_joystick.setYAxis(axisSetting);
+      m_joystick.setZAxis(axisSetting);
+      m_joystick.setRxAxis(axisSetting);
+      m_joystick.setRyAxis(axisSetting);
+      m_joystick.setRzAxis(axisSetting);
+      m_joystick.setThrottle(axisSetting);
+      m_joystick.setRudder(axisSetting);
+      m_joystick.setSteering(axisSetting);
+      m_joystick.setAccelerator(axisSetting);
+      m_joystick.setBrake(axisSetting);
       needUpdate = true;
     }
   }
--- console/src/main/java/frc/robot/Robot.java
+++ console/src/main/java/frc/robot/Robot.java
@@ -1,31 +1,40 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public final class Robot extends TimedRobot {
+    private final Joystick m_joystick = new Joystick(0);
 
-  // private final GenericHID m_joystick = new GenericHID(0);
+    public Robot() {
+        System.out.println(m_joystick.getName());
+        System.out.println("axis count: " + m_joystick.getAxisCount());
+        System.out.println("button count: " + m_joystick.getButtonCount());
+    }
 
-  private final Joystick m_joystick = new Joystick(0);
-  // button index is one-based
-  private final JoystickButton m_button0 = new JoystickButton(m_joystick, 1);
+    @Override
+    public void teleopPeriodic() {
+        System.out.printf(
+                "axes: %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n",
+                ax(0), ax(1), ax(2), ax(3), ax(4), ax(5), ax(6), ax(7), ax(8));
+        System.out.printf(
+                "buttons: %b  %b %b %b %b %b %b %b"
+                        + " %b %b %b %b %b %b %b %b"
+                        + " %b %b %b %b %b %b %b %b"
+                        + " %b %b %b %b %b %b %b %b\n",
+                bu(1), bu(2), bu(3), bu(4), bu(5), bu(6), bu(7), bu(8),
+                bu(9), bu(10), bu(11), bu(12), bu(13), bu(14), bu(15), bu(16),
+                bu(17), bu(18), bu(19), bu(20), bu(21), bu(22), bu(23), bu(24),
+                bu(25), bu(26), bu(27), bu(28), bu(29), bu(30), bu(31), bu(32));
 
-  public Robot() {
-    // simgui can't see the buttons.
-    m_button0.whenPressed(() -> System.out.println("foo"), m_noting);
-  }
+        // m_joystick.setOutput(0, true);
+    }
 
-  @Override
-  public void teleopPeriodic() {
+    private double ax(int a) {
+        return m_joystick.getRawAxis(a);
+    }
 
-    //// System.out.println(m_joystick.getName());
-    // System.out.println(DriverStation.getJoystickName(0));
-    System.out.println(DriverStation.getStickButtonCount(0)); // zero in sim
-
-  }
-
+    private boolean bu(int b) {
+        return m_joystick.getRawButton(b);
+    }
 }
