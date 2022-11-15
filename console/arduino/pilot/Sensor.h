a77c96f90057e122c7740cf4208a79f83334f8a1
Joel Truher <joel@truher.org>
2022-11-14 18:39:25 -0800

    hmm, key thing doesn't work?  not done here

--- console/arduino/pilot/Sensor.h
+++ console/arduino/pilot/Sensor.h
@@ -46,9 +46,9 @@ public:
    * TODO: do something in the case where this fails.
    */
   void initialize() {
-    if (!neokey0.begin(0x30)) {
-      return;
-    }
+    // if (!neokey0.begin(0x30)) {
+    //   return;
+    // }
     if (!encoder0.begin(0x36)) {
       return;
     }
@@ -62,7 +62,7 @@ public:
     encoder1.pinMode(SS_SWITCH, INPUT_PULLUP);
     encoder2.pinMode(SS_SWITCH, INPUT_PULLUP);
 
-    // TODO: is this necessary?
+    // // TODO: is this necessary?
     encoder0.setGPIOInterrupts((uint32_t)1 << SS_SWITCH, 1);
     encoder1.setGPIOInterrupts((uint32_t)1 << SS_SWITCH, 1);
     encoder2.setGPIOInterrupts((uint32_t)1 << SS_SWITCH, 1);
@@ -71,12 +71,12 @@ public:
     encoder1.enableEncoderInterrupt();
     encoder2.enableEncoderInterrupt();
 
-    if (!adcSensor.begin(0x48)) {
-      return;
-    }
+    // if (!adcSensor.begin(0x48)) {
+    //   return;
+    // }
 
-    mysensor.begin();
-    mysensor.setZeroReg();
+    // mysensor.begin();
+    // mysensor.setZeroReg();
 
     initialized = true;
   }
@@ -97,39 +97,97 @@ public:
    * Reads all the sensors and writes the values into reportTx.
    */
   void sense(ReportTx& reportTx) {
-    uint32_t buttons0 = ~neokey0.digitalReadBulk(NEOKEY_1X4_BUTTONMASK);
-    Keys k0 = *(Keys*)&buttons0;
-    reportTx.b1 = k0.a;
-    reportTx.b2 = k0.b;
-    reportTx.b3 = k0.c;
-    reportTx.b4 = k0.d;
+    // uint32_t buttons0 = ~neokey0.digitalReadBulk(NEOKEY_1X4_BUTTONMASK);
+    // Keys k0 = *(Keys*)&buttons0;
+    // reportTx.b1 = k0.a;
+    // reportTx.b2 = k0.b;
+    // reportTx.b3 = k0.c;
+    // reportTx.b4 = k0.d;
 
-    reportTx.b5 = encoder0.digitalRead(SS_SWITCH);
-    reportTx.b6 = encoder1.digitalRead(SS_SWITCH);
-    reportTx.b7 = encoder2.digitalRead(SS_SWITCH);
+    reportTx.b5 = not encoder0.digitalRead(SS_SWITCH);
+    reportTx.b6 = not encoder1.digitalRead(SS_SWITCH);
+    reportTx.b7 = not encoder2.digitalRead(SS_SWITCH);
 
-    // use "spare" axes for the encoders.
-    // note the axes are int16_t, the encoder is int32_t,
-    // but overflow would require >1000 revolutions, will
-    // never happen.
-    reportTx.rx = encoder0.getEncoderPosition();
+    // // use "spare" axes for the encoders.
+    // // note the axes are int16_t, the encoder is int32_t,
+    // // but overflow would require >1000 revolutions, will
+    // // never happen.
+    reportTx.rz = encoder0.getEncoderPosition();
     reportTx.slider = encoder1.getEncoderPosition();
     reportTx.dial = encoder2.getEncoderPosition();
 
-    reportTx.x = adcSensor.getSingleEnded(0);
-    reportTx.y = adcSensor.getSingleEnded(1);
+    // reportTx.x = adcSensor.getSingleEnded(0);
+    // reportTx.y = adcSensor.getSingleEnded(1);
 
-    reportTx.rz = mysensor.angleR(U_TRN, true);  // turns
+    // reportTx.rz = mysensor.angleR(U_TRN, true);  // turns
+  }
+
+  /**
+   * Lights the keys as specified in ReportRx.
+   *
+   * The key mapping here matches the one in sense(), as a demo.
+   * In general, these outputs might be more useful if they represented
+   * more than just a keypress.
+   */
+  void indicate(ReportRx rpt) {
+    // if (rpt == prev) {  // speed up the common case
+    //   return;
+    // }
+    // lite(neokey0, 0, 0x00ffff, rpt.i1, prev.i1);  // a
+    // lite(neokey0, 1, 0x00ffff, rpt.i2, prev.i2);  // b
+    // lite(neokey0, 2, 0x00ffff, rpt.i3, prev.i3);  // c
+    // lite(neokey0, 3, 0x00ffff, rpt.i4, prev.i4);  // d
+    // neokey0.pixels.show();
+    // prev = rpt;
+  }
+
+  /**
+   * A little light show to show it's working.
+   */
+  void splash() {
+    // for (int i = 0; i < 4; i++) {
+    //   neokey0.pixels.setPixelColor(i, 0xffffff);
+    //   neokey0.pixels.show();
+    //   delay(25);
+    // }
+    // for (int i = 0; i < 4; i++) {
+    //   neokey0.pixels.setPixelColor(i, 0x000000);
+    //   neokey0.pixels.show();
+    //   delay(25);
+    // }
+    // for (int i = 0; i < 256; i += 16) {
+    //   for (int k = 0; k < 4; ++k) {
+    //     neokey0.pixels.setPixelColor(k, i, 0, 0);
+    //   }
+    //   neokey0.pixels.show();
+    //   delay(25);
+    // }
+    // delay(750);
+    // for (int i = 0; i < 4; i++) {
+    //   neokey0.pixels.setPixelColor(i, 0x000000);
+    // }
+    // neokey0.pixels.show();
   }
 
 private:
-  Adafruit_NeoKey_1x4 neokey0;
+  //Adafruit_NeoKey_1x4 neokey0; // broken?  get another one
+  //ReportRx prev;
   Adafruit_seesaw encoder0;
   Adafruit_seesaw encoder1;
   Adafruit_seesaw encoder2;
-  ADS1015 adcSensor;
-  AMS_AS5048B mysensor{ 0x40 };
+  //ADS1015 adcSensor;
+  //AMS_AS5048B mysensor{ 0x40 };
   // TODO: do something with initialized (e.g. report an error if it's false)
   bool initialized{};
+
+  /**
+   * Changes the state of one key, if the new state is different.
+   */
+  void lite(Adafruit_NeoKey_1x4& key, int i, uint32_t color, bool state, bool previousState) {
+    if (state == previousState) {
+      return;
+    }
+    key.pixels.setPixelColor(i, state ? color : 0x000000);
+  }
 };
 #endif  // SENSOR_H
--- console/arduino/pilot/pilot.ino
+++ console/arduino/pilot/pilot.ino
@@ -8,11 +8,32 @@ Transceiver transceiver_(Transceiver::SubConsole::PILOT, reportRx_);
 Sensor sensor_;
 
 void setup() {
+  Serial.begin(115200);
+while (! Serial) delay(10);
+  Serial.println("init");
   sensor_.initialize();
+  Serial.println("init done");
+  sensor_.splash();
+ pinMode(17, OUTPUT);
 }
 
-/** Ignores outputs, there's nowhere to display them */
+
 void loop() {
+  // Serial.println("loop");
+  // delay(100);
+  //   digitalWrite(17, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // delay(1000);                       // wait for a second
+  // digitalWrite(17, LOW);    // turn the LED off by making the voltage LOW
+  // delay(1000); 
   sensor_.sense(reportTx_);
   transceiver_.send(reportTx_);
+  ///////////////////////
+  // for this demo we loop back.
+  // TODO: take this out, do it in the RIO.
+  ///////////////////////
+ // reportRx_ = *(ReportRx*)((char*)(&reportTx_) + 16);
+  ///////////////////////
+  // TODO: remove the above line
+  ///////////////////////
+ // sensor_.indicate(reportRx_);
 }
