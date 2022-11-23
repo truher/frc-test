d25c0ac9229b52780cf795ac5b5aefa1d8206997
Joel Truher <joel@truher.org>
2022-11-23 08:43:36 -0800

    add RTTTL and amplifier libraries.
    note autopilot doesn't compile due to transceiver changes; i'll fix that...

--- console/arduino/autopilot/autopilot.ino
+++ console/arduino/autopilot/autopilot.ino
@@ -3,15 +3,49 @@
 #include "Sensor.h"
 #include "Indicator.h"
 
+#include <NonBlockingRtttl.h>
+// see https://github.com/end2endzone/NonBlockingRTTTL
+#include <SparkFun_TPA2016D2_Arduino_Library.h>
+// see https://github.com/sparkfun/SparkFun_TPA2016D2_Arduino_Library
+
+#define BUZZER_PIN 8
+// for now just demonstrate the sound part.
+// TODO: play useful sounds
+const char * tetris = "tetris:d=4,o=5,b=160:e6,8b,8c6,8d6,16e6,16d6,8c6,8b,a,8a,8c6,e6,8d6,8c6,b,8b,8c6,d6,e6,c6,a,2a,8p,d6,8f6,a6,8g6,8f6,e6,8e6,8c6,e6,8d6,8c6,b,8b,8c6,d6,e6,c6,a,a";
+
+TPA2016D2 amp;
+
 Data data_;
 Transceiver transceiver_(Transceiver::SubConsole::AUTOPILOT, data_);
 Sensor sensor_(data_);
 Indicator indicator_(data_);
 
 void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  Wire.begin();
+  if (amp.begin() == false) {
+    while (1)
+      ;
+  }
+  // for now just turn it on
+  // TODO: adjust gain with knob
+  amp.enableRightSpeaker();
+  amp.enableLeftSpeaker();
+  amp.disableLimiter();
+  amp.disableNoiseGate();
+  amp.writeRelease(1);
+  amp.writeAttack(1);
+
+  amp.writeFixedGain(15);  // half
 }
 
 void loop() {
+  if (rtttl::isPlaying()) {
+    rtttl::play();
+  } else {
+    rtttl::begin(BUZZER_PIN, tetris);
+  }
   if (sensor_.sense()) transceiver_.send();
   if (transceiver_.recv()) indicator_.indicate();
 }
