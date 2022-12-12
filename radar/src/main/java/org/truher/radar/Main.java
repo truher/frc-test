249a2b5afa4a4c4a046ce156534f55490d3ed872
Joel Truher <joel@truher.org>
2022-12-11 17:15:39 -0800

    functional NT4 client app

--- radar/src/main/java/org/truher/radar/Main.java
+++ radar/src/main/java/org/truher/radar/Main.java
@@ -1,40 +1,31 @@
-package edu.wpi.first.shuffleboard.app;
-
-import com.sun.javafx.application.LauncherImpl;
+package org.truher.radar;
 
 import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.BooleanTopic;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.shuffleboard.plugin.networktables.NetworkTablesPlugin;
 import edu.wpi.first.util.CombinedRuntimeLoader;
 import edu.wpi.first.util.WPIUtilJNI;
 
 import java.io.IOException;
 
-
 public final class Main {
 
-
-
-  public static void main(String[] args) throws IOException {
-
+  public static void main(String[] args) throws IOException, InterruptedException {
 
     WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
     CombinedRuntimeLoader.loadLibraries(Main.class, "wpiutiljni");
 
+    System.out.println("##############################################################################");
+    System.out.println("##############################################################################");
+    System.out.println("##############################################################################");
+    System.out.println("##############################################################################");
+    System.out.println("##############################################################################");
 
-    System.out.println("##############################################################################");
-    System.out.println("##############################################################################");
-    System.out.println("##############################################################################");
-    System.out.println("##############################################################################");
-    System.out.println("##############################################################################");
-    
-
-
-    NetworkTablesPlugin ntp = new NetworkTablesPlugin(); // side-effect = loads ntcore
+    NetworkTablesPlugin ntp = new NetworkTablesPlugin();
+    ntp.onLoad();
     NetworkTable t = NetworkTableInstance.getDefault().getTable("");
-    BooleanTopic  b = t.getBooleanTopic("blarg2");
+    BooleanTopic b = t.getBooleanTopic("blarg2");
     BooleanPublisher p = b.publish();
     p.set(true);
 
@@ -43,14 +34,7 @@ public final class Main {
     System.out.println("##############################################################################");
     System.out.println("##############################################################################");
     System.out.println("##############################################################################");
-    
 
-    
-
-
-    System.setProperty("jdk.gtk.version", "2");
-
-
-    LauncherImpl.launchApplication(Shuffleboard.class, null, args);
+    Thread.sleep(30000);
   }
 }
