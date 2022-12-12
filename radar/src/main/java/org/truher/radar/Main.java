b610b339955ec7f869769a0047c3deab45c9f9c7
Joel Truher <joel@truher.org>
2022-12-11 22:34:16 -0800

    cleanups

--- radar/src/main/java/org/truher/radar/Main.java
+++ radar/src/main/java/org/truher/radar/Main.java
@@ -4,6 +4,7 @@ import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.BooleanTopic;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.util.CombinedRuntimeLoader;
 import edu.wpi.first.util.WPIUtilJNI;
 
@@ -16,20 +17,19 @@ public final class Main {
     WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
     CombinedRuntimeLoader.loadLibraries(Main.class, "wpiutiljni");
 
-    System.out.println("START ##############################################################################");
+    NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+    // json filename matches build.gradle
+    var files = CombinedRuntimeLoader.extractLibraries(Main.class, "/ResourceInformation-NetworkTables.json"); 
+    CombinedRuntimeLoader.loadLibrary("ntcorejni", files);
 
-    NetworkTablesPlugin ntp = new NetworkTablesPlugin();
-    ntp.onLoad();
-    NetworkTable t = NetworkTableInstance.getDefault().getTable("");
-    BooleanTopic b = t.getBooleanTopic("blarg2");
-    BooleanPublisher p = b.publish();
-    p.set(true);
+    NetworkTableInstance inst =  NetworkTableInstance.getDefault(); 
+    inst.setServer("localhost", NetworkTableInstance.kDefaultPort4);
+    inst.startClient4("radar");
+    inst.startDSClient();
 
     TargetSubscriber r = new TargetSubscriber();
     r.run();
     new TargetPublisher().run();
-
-    System.out.println("END ##############################################################################");
 
     Thread.sleep(30000);
   }
