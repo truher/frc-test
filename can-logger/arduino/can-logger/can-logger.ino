058508d4722ad2d5f10eef0489fffc4b9b19a1de
Joel Truher <joel@truher.org>
2022-12-27 21:23:12 -0800

    start to do the sd writing

--- can-logger/arduino/can-logger/can-logger.ino
+++ can-logger/arduino/can-logger/can-logger.ino
@@ -5,10 +5,29 @@
  *
  * TODO: write to SD.
  */
-#include <FlexCAN_T4.h>
+#include "FlexCAN_T4.h"
+#include "SdFat.h"
+#include "RingBuf.h"
+
+// Use Teensy SDIO
+#define SD_CONFIG  SdioConfig(FIFO_SDIO)
+
+// Size to log 10 byte lines at 25 kHz for more than ten minutes.
+#define LOG_FILE_SIZE 10*20000*600  // 150,000,000 bytes.
+
+// Space to hold more than 800 ms of data for 10 byte lines at 25 ksps.
+#define RING_BUF_CAPACITY 400*512
+#define LOG_FILENAME "SdioLogger.csv"  // TODO: unique filenames
+
 bool ledState = false;
 FlexCAN_T4<CAN0, RX_SIZE_256, TX_SIZE_16> can0;
 int packets = 0;
+
+SdFs sd;
+FsFile file;
+
+// RingBuf for File type FsFile.
+RingBuf<FsFile, RING_BUF_CAPACITY> rb;
 
 void setup(void) {
   Serial.begin(9600);
